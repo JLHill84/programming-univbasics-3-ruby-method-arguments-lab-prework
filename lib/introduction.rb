82184298ed40d80a68ddbca45d3fa987904faa5b
Joshua Hill <josh@jlhworkshop.com>
2019-07-29 20:50:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,5 +9,5 @@ end
 def introduction_with_language_optional(name, language)
   if language == nil then language = "Ruby"
     puts "Hi, my name is #{name} and I am learning to program in #{language}"
-end
-  
+  end
+end  
