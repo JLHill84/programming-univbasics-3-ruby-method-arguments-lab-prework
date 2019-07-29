c537eb8d0620f65970251533cbe928a1c68e623a
Joshua Hill <josh@jlhworkshop.com>
2019-07-29 20:51:37 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,13 +1,13 @@
 def introduction(name)
-  puts "Hi, my name is #{name}\n"
+  puts "Hi, my name is #{name}.\n"
 end
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}\n"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
 def introduction_with_language_optional(name, language)
   if language == nil then language = "Ruby"
-    puts "Hi, my name is #{name} and I am learning to program in #{language}"
+    puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
   end
 end  
