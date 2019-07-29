b94521e518919b8225a09228bacc5c8bf57dd455
Joshua Hill <josh@jlhworkshop.com>
2019-07-29 21:01:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,8 +7,8 @@ def introduction_with_language(name, language)
 end
 
 def introduction_with_language_optional(name, language)
-  if language == "" then language = "Ruby"
-  else language = language
-    puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+  lang = language
+  if lang == "" then lang = "Ruby"
   end
+  puts "Hi, my name is #{name} and I am learning to program in #{lang}.\n"
 end  
